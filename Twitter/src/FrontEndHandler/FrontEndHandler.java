02b5cce548d3c8a51d33196c90fd5c59af0919eb
siyuanlyn <siyuanlyn@gmail.com>
2014-03-18 17:20:37 -0400

    Correctly response heartbeat information

--- Twitter/src/FrontEndHandler/FrontEndHandler.java
+++ Twitter/src/FrontEndHandler/FrontEndHandler.java
@@ -1,6 +1,8 @@
 package FrontEndHandler;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,9 +17,9 @@ public class FrontEndHandler extends HttpServlet{
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		PrintWriter out = response.getWriter();
-		out.println("Test");
-		String p1 = request.getParameter("userid");
-		out.println(p1);
+		out.println("lmao, " + "0992-3171-4790");
+		Date date = new Date();
+		out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date));
 	}
 
 }
